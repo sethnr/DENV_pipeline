1954abfa315fca36d7e558d09e77bd1cf28976a9
seth redmond <seth.redmond@gmail.com>
2023-09-11 13:25:52 -0400

    setting in/out for caller script

--- snakemake/scripts/caller.sh
+++ snakemake/scripts/caller.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cores=1
-while getopts "n:s:c:e:d:p:t:C:T:L:" OPTION; do
+while getopts "n:p:s:e:c:d:t:C:O:I:L:" OPTION; do
     case $OPTION in
     n) fname=$OPTARG    ;;
     p) primer_dir=$OPTARG ;;
@@ -17,9 +17,10 @@ while getopts "n:s:c:e:d:p:t:C:T:L:" OPTION; do
     d) depth=$OPTARG ;;
     t) threshold=$OPTARG ;;
 
-    #threading / tempdirs
+    #threading / outdirs
     C) cores=$OPTARG ;;
-    T) tempdir=$OPTARG ;;
+    O) outdir=$OPTARG ;;
+    I) indir=$OPTARG ;;
     L) log=$OPTARG ;;
     *)  echo "option not recognised"
         exit 1
@@ -46,31 +47,31 @@ while IFS= read -r virustype || [[ -n "$virustype" ]]; do
     echo "----->>>>>Calling variants against "${virustype}" reference sequence"
 
     echo "----->>>>>Generating consensus sequence"
-    samtools mpileup -aa --reference ${fasta} -A -d 10000 -Q 0 ${tempdir}/${fname}.${virustype}.sort.bam | ivar consensus -t ${threshold} -m ${depth} -p ${tempdir}/${fname}.${virustype}.${depth}.cons -i ${consensus_name} >> ${log} 2>&1
+    samtools mpileup -aa --reference ${fasta} -A -d 10000 -Q 0 ${indir}/${fname}.${virustype}.sort.bam | ivar consensus -t ${threshold} -m ${depth} -p ${outdir}/${fname}.${virustype}.cons -i ${consensus_name} >> ${log} 2>&1
     
     echo "----->>>>>Aligning consensus cps sequence against the reference serotype "${virustype}" cps sequence"
-    nextalign run  --reference ${fasta} --output-fasta ${tempdir}/${fname}.${virustype}.${depth}.out.aln ${tempdir}/${fname}.${virustype}.${depth}.cons.fa >> ${log} 2>&1
+    nextalign run  --reference ${fasta} --output-fasta ${outdir}/${fname}.${virustype}.out.aln ${outdir}/${fname}.${virustype}.cons.fa >> ${log} 2>&1
     
-    if [ -s ${tempdir}/${fname%.*}.${virustype}.${depth}.out.aln ]; then
+    if [ -s ${outdir}/${fname%.*}.${virustype}.out.aln ]; then
         echo "----->>>>>Aligning with nextalign successful"
     else
         echo "----->>>>>Aligning with mafft (nextalign not successful)" 
-        mafft --quiet --6merpair --keeplength  --addfragments ${tempdir}/${fname}.${virustype}.${depth}.cons.fa ${fasta} > ${tempdir}/${fname}.${virustype}.${depth}.alignment_intermediate.fasta
-        grep -A 30000000 ">${consensus_name}" ${tempdir}/${fname}.${virustype}.${depth}.alignment_intermediate.fasta > ${tempdir}/${fname}.${virustype}.${depth}.out.aln
+        mafft --quiet --6merpair --keeplength  --addfragments ${outdir}/${fname}.${virustype}.cons.fa ${fasta} > ${outdir}/${fname}.${virustype}.alignment_intermediate.fasta
+        grep -A 30000000 ">${consensus_name}" ${outdir}/${fname}.${virustype}.alignment_intermediate.fasta > ${outdir}/${fname}.${virustype}.out.aln
     fi
 
     echo "----->>>>>Calculating percentage coverage against serotype "${virustype}" cps reference sequence"
     which python
     if [ -s ${trimbed} ]; then
-        $CONDA_PREFIX/bin/python ${serotype_caller} --alignment ${tempdir}/${fname}.${virustype}.${depth}.out.aln --bed-file ${trimbed} --outfile ${tempdir}/${fname}_all_virustype_info.txt
+        $CONDA_PREFIX/bin/python ${serotype_caller} --alignment ${outdir}/${fname}.${virustype}.out.aln --bed-file ${trimbed} --outfile ${outdir}/${fname}_all_virustype_info.txt
     else
-        $CONDA_PREFIX/bin/python ${serotype_caller}  --alignment ${tempdir}/${fname}.${virustype}.${depth}.out.aln --outfile ${tempdir}/${fname}_all_virustype_info.txt
+        $CONDA_PREFIX/bin/python ${serotype_caller}  --alignment ${outdir}/${fname}.${virustype}.out.aln --outfile ${outdir}/${fname}_all_virustype_info.txt
     fi
 
     echo "----->>>>>Identifying variants"
-    samtools mpileup -aa --reference ${fasta} -A -d 0 -Q 0 ${tempdir}/${fname}.${virustype}.sort.bam | ivar variants -p ${tempdir}/${fname}.${virustype}.${depth}.variants -q 20 -t 0.03 -r ${fasta} >> ${log}     
+    samtools mpileup -aa --reference ${fasta} -A -d 0 -Q 0 ${indir}/${fname}.${virustype}.sort.bam | ivar variants -p ${outdir}/${fname}.${virustype}.variants -q 20 -t 0.03 -r ${fasta} >> ${log}     
     echo "----->>>>>>Getting depths"
-    bedtools genomecov -d -ibam ${tempdir}/${fname}.${virustype}.sort.bam > ${tempdir}/${fname}.${virustype}.depth.txt; 
+    bedtools genomecov -d -ibam ${indir}/${fname}.${virustype}.sort.bam > ${outdir}/${fname}.${virustype}.depth.txt; 
 
     echo "--->>>>> Finished"
 
